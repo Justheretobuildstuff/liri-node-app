f15990bfe5f14235299577cf8edd93be2ced2392
Josh Daniels <joshdaniels@IMac.local>
2018-08-29 20:40:00 -0700

    everything should be up to date

--- liri.js
+++ liri.js
@@ -7,15 +7,32 @@ const keys = require('./keys.js');
 
 var userInput = process.argv[2];
 
+var client = new Twitter({
+    consumer_key: keys.twitter.consumer_key,
+    consumer_secret: keys.twitter.consumer_secret,
+    access_token_key: keys.twitter.access_token_key,
+    access_token_secret: keys.twitter.access_token_secret
+  });
+   
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+  });
+
+  /*
 if (userInput = "my-tweets") {
-    request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=" + consumer_key, function (error, response, body) {
+    request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
         // If the request was successful...
         if (!error && response.statusCode === 200) {
 
             // Then log the body from the site!
-            console.log(consumer_key);
+            console.log(keys.twitter.access_token_key);
         }
     });
 
 };
+
+*/
